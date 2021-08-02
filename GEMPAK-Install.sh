5f7fa011621bd7c5aa02efb8f6b1ec0a1186dc9e
Will H <80460171+whatheway@users.noreply.github.com>
2021-08-02 16:51:05 -0500

    Add files via upload

--- GEMPAK-Install.sh
+++ GEMPAK-Install.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-#Basic Package Management for GEMPAK
+
+
+################# Creating folders for GEMPAK ##################################
+
+export HOME=`cd;pwd`
+mkdir $HOME/GEMPAK
+mkdir $HOME/GEMPAK/Downloads
+
+
+####################### Basic Package Management for GEMPAK #####################
 
 sudo apt-get update
 sudo apt-get upgrade
@@ -10,6 +19,8 @@ sudo apt-get install make autoconf automake libtool gcc build-essential gfortran
 
 
 #Downloading old packages for GEMPAK that are no longer in Apt library
+cd $HOME/GEMAPK/Downloads
+
 #Libfortran 3.6.5.0
 wget http://archive.ubuntu.com/ubuntu/pool/universe/g/gcc-6/libgfortran3_6.5.0-2ubuntu1~18.04_amd64.deb
 #GCC-6
@@ -42,7 +53,67 @@ sudo dpkg -i libmotif-dev_2.3.8-2build1_amd64.deb
 echo "Installing LIBXP-DEV"
 sudo dpkg -i libxp-dev_1.0.2-2_amd64.deb
 
+################### Download GEMPAK tarball and untar into GEMPAK folder #####################
 
-#Creating folders for GEMPAK
-mkdir $HOME/GEMPAK
+wget -c https://www.unidata.ucar.edu/downloads/gempak/nawips-7.5.1/gempak-7.5.1.tar.gz
+tar -xvzf gempak-7.5.1.tar.gz -C $HOME/GEMPAK
+
+################### Creating symbolic link to GEMPAK7 to NAWIPS ##############################
+
+
+cd $HOME/GEMPAK
+ln -s GEMPAK7/ NAWIPS
+
+
+
+###################### Edit Gemenviron & Gemenviron.profile #################################
+#Using uncommon delimiters for sed
+
+
+cd $HOME/GEMPAK/GEMPAK7
+sed -i "s|setenv NAWIPS /home/gempak/GEMPAK7|setenv NAWIPS $HOME/GEMPAK/GEMPAK7|" Gemenviron
+sed -i "s|NAWIPS=/home/gempak/GEMPAK7|NAWIPS=$HOME/GEMPAK/GEMPAK7|" Gemenviron.profile
+
+
+####################### Sourcing Environmental Variables ###########################
+# Adding path of GEMPAK to /.bashrc $ /.profile
+#This will be done with the cat command
+#In terminal enter cat --help to understand options
+
+
+cd $HOME
+cat >> .bashrc <<'EOF'
+. /$HOME/GEMPAK/NAWIPS/Gemenviron.profile
+EOF
+source .bashrc                                #sourcing .bashrc to enable path.
+
+cat >> .profile <<'EOF'
+. /$HOME/GEMPAK/NAWIPS/Gemenviron.profile
+EOF
+source .porfile                              #sourcing .profile to enable path.
+
+
+##################################### Installing GEMPAK 7.5.1 #################################
+# 
+
+cd $HOME/GEMPAK/NAWIPS
+make all >& make.out & tail -f make.out    # Invoking Make to compile GEMPAK7.5.1 w/ tail output.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
