5324a2aa0c0e6a3384668969b0812f6f377966b4
Will H <80460171+whatheway@users.noreply.github.com>
2022-04-27 11:10:55 -0500

    Update GEMPAK-Install.sh

--- GEMPAK-Install.sh
+++ GEMPAK-Install.sh
@@ -14,24 +14,26 @@ mkdir $HOME/GEMPAK/Downloads
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo apt-get install make autoconf automake libtool gcc build-essential gfortran libx11-dev libxt-dev libxext-dev libmotif-dev libxft-dev libxtst-dev xorg xbitmaps flex byacc locate libmotif-common libmotif-dev xfonts-100dpi xfonts-75dpi xfonts-cyrillic libxpm4 libxpm-dev
+sudo apt-get install make autoconf automake libtool gcc build-essential gfortran libx11-dev libxt-dev libxext-dev libmotif-dev libxft-dev libxtst-dev xorg xbitmaps flex byacc locate libmotif-common libmotif-dev xfonts-100dpi xfonts-75dpi xfonts-cyrillic libxpm4 libxpm-dev gfortran-8
 
 ################### Download GEMPAK tarball and untar into GEMPAK folder #####################
+cd $HOME/GEMPAK/Downloads
 wget https://github.com/Unidata/gempak/archive/refs/tags/7.14.0.1.tar.gz
-tar -xvzf gempak-7.14.0.1.tar.gz -C $HOME/GEMPAK
+tar -xvzf 7.14.0.1.tar.gz -C $HOME/GEMPAK
 
 ################### Creating symbolic link to GEMPAK7 to NAWIPS ##############################
 
 cd $HOME/GEMPAK
-ln -s GEMPAK7/ NAWIPS
+ln -s gempak-7.14.0.1/ NAWIPS
 
 ###################### Edit Gemenviron & Gemenviron.profile #################################
 #Using uncommon delimiters for sed
+#Changing default path for GEMPAK to current location
 
 
-cd $HOME/GEMPAK/GEMPAK7
-sed -i "s|setenv NAWIPS /home/gempak/GEMPAK7|setenv NAWIPS $HOME/GEMPAK/GEMPAK7|" Gemenviron
-sed -i "s|NAWIPS=/home/gempak/GEMPAK7|NAWIPS=$HOME/GEMPAK/GEMPAK7|" Gemenviron.profile
+cd $HOME/GEMPAK/gempak-7.14.0.1
+sed -i "s|setenv NAWIPS /home/gempak/GEMPAK7|setenv NAWIPS $HOME/GEMPAK/gempak-7.14.0.1|" Gemenviron
+sed -i "s|NAWIPS=/home/gempak/GEMPAK7|NAWIPS=$HOME/GEMPAK/gempak-7.14.0.1|" Gemenviron.profile
 
 
 ####################### Sourcing Environmental Variables ###########################
@@ -41,6 +43,7 @@ sed -i "s|NAWIPS=/home/gempak/GEMPAK7|NAWIPS=$HOME/GEMPAK/GEMPAK7|" Gemenviron.p
 
 
 cd $HOME
+
 cat >> .bashrc <<'EOF'
 . /$HOME/GEMPAK/NAWIPS/Gemenviron.profile
 EOF
@@ -49,19 +52,39 @@ source .bashrc                                #sourcing .bashrc to enable path.
 cat >> .profile <<'EOF'
 . /$HOME/GEMPAK/NAWIPS/Gemenviron.profile
 EOF
-source .porfile                              #sourcing .profile to enable path.
+source .profile                              #sourcing .profile to enable path.
 
 
-##################################### Installing GEMPAK 7.5.1 #################################
-# 
+################################## Installing GEMPAK 7.10.0.1 #################################
+# Linking Make files to proper locations
+# Adjusting FC variable from default gfortran to gfortran-8
+
+
+cd $HOME/GEMPAK/NAWIPS/config
+mv Makeinc.linux64_gfortran Makeinc.linux64_gfortran.orig
+ln -s Makeinc.linux64_gfortran_ubuntu Makeinc.linux64_gfortran
+
+sed -i 's/FC = gfortran/FC = gfortran-8/p' Makeinc.linux64_gfortran_ubuntu
+
 
 cd $HOME/GEMPAK/NAWIPS
-make all >& make.out & tail -f make.out    # Invoking Make to compile GEMPAK7.14.0.1 w/ tail output.
-make programs_nc
-make programs_gf
+echo 'Make all in progress.'
+echo 'To see code, in new terminal enter (tail -f make.out) in $HOME/GEMPAK/NAWIPS'
+make all >& make.out    # Invoking Make to compile GEMPAK7.14.0.1 w/ tail output.
+echo 'Make programs_nc in progress'
+echo 'To see code, in new terminal enter (tail -f make.out.nc) in $HOME/GEMPAK/NAWIPS'
+make programs_nc >& make.out.nc 
+echo 'Make programs_gf in progress'
+echo 'To see code, in new terminal enter (tail -f make.out.gf) in $HOME/GEMPAK/NAWIPS'
+make programs_gf >& make.out.gf
+echo 'Make install in progress'
+echo 'To see code, in new terminal enter (tail -f make.out.install) in $HOME/GEMPAK/NAWIPS'
+make install >& make.out.install
 
-Make install
+echo 'GEMPAK INSTALLED'
 
+echo 'Thank you for using my GEMPAK install script'
+echo 'Special thanks to github users akrherz & mzuranski'
 
 
 
